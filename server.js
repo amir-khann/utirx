987aba6561a83f3631d4ada3e56fd3a2537cdfc5
Murtaz Kazmi <murtaz@logmethod.com>
2021-09-01 16:20:57 +0500

    server file added

---
+++ server.js
@@ -0,0 +1,11 @@
+// express app to serve react build files on / route
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
