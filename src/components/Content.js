5973c104966cc0899754d6732e4cd6dbfb1f1832
tr-zaziz <zaziz@technologyrivers.com>
2022-03-15 12:14:31 +0500

    content changes done

--- src/components/Content.js
+++ src/components/Content.js
@@ -21,18 +21,18 @@ const Content = ({ navigate }) => {
         <div className="left">
           <h1>Get your UTI Treated Today</h1>
           <p>Got a UTI ? </p>
-          <p>Get your UTI diagnosed online right now and get a subscription for antibiotics sent straight to your pharmacy - all without the hassle of going to a doctor’s office. </p>
+          <p>Get your UTI diagnosed online right now and get a subscription for antibiotics sent straight to your pharmacy within 24 hours all without the hassle of going to a doctor’s office. </p>
           <ul className="checked-list">
             {/*<li>Got a UTI?</li>*/}
             <li>Don’t have time to wait for an appointment with your doc?</li>
-            <li>Talk to a provider within minutes</li>
-            <li>Pick up your prescription same-day at your pharmacy or get it discreetly delivered</li>
+            <li>Get connected to a provider within minutes</li>
+            <li>Pick up your prescription within 24 hours if qualified at your pharmacy.</li>
             <li>It’s fast. It’s easy. There are no hidden fees. And there’s no appointment necessary.</li>
             <li>No insurance needed. It’s affordable, fast, and easy.</li>
-            <li>Get your UTI diagnosed online right now and get a subscription for antibiotics sent straight to your pharmacy - all without the hassle of going to a doctor’s office.</li>
+            {/*<li>Get your UTI diagnosed online right now and get a subscription for antibiotics sent straight to your pharmacy - all without the hassle of going to a doctor’s office.</li>*/}
           </ul>
           <b>${price} single visit</b>
-          <b>Ages 18-65 • Female</b>
+          {/*<b>Ages 18-65 • Female</b>*/}
           <b className="message-fl">This application is only available to Florida residents and visitors and prescription will be sent to Florida Pharmacies only.</b>
           <Button type="primary" style={{ minWidth: '200px', borderRadius: '5px', marginTop: '10px' }} onClick={() => navigate()}>Get Started</Button>
         </div>
@@ -46,20 +46,22 @@ const Content = ({ navigate }) => {
         <div className="flex wrap column-on-mobile">
           <div className="left">
             <Card style={{textAlign: 'left', marginRight: '5px'}}>
-              <h4>How Can Online UTI Meds Help with UTIs?</h4>
-              <p>Most UTIs respond quickly to treatment with an appropriate antibiotic. Online UTI Meds can connect you with providers who can prescribe antibiotics. The exact treatment they prescribe depends on your diagnosis and risk factors. Generally, they prescribe an oral antibiotic like</p>
+              <h4>How Can Online UTIRX.com Help with UTIs?</h4>
+              <p>Most UTIs respond quickly to treatment with an appropriate antibiotic. UTIRX.com connect you with providers who can prescribe antibiotics. The exact treatment they prescribe depends on your diagnosis and history.</p>
+              <span>Common Antibiotics Include:</span>
               <ul>
                 <li>Macrobid (Nitrofurantoin)</li>
                 <li>Keflex (Cephalexin)</li>
                 <li>Bactrim (Sulfamethoxazole / Trimethoprim)</li>
               </ul>
-              <p>They can also provide advice on how to prevent UTIs in the future.</p>
+              {/*<p>They can also provide advice on how to prevent UTIs in the future.</p>*/}
             </Card>
           </div>
           <div className="right">
             <Card style={{textAlign: 'left', height: "100%"}}>
               <h4>Is This Service Right For Me?</h4>
               <p>Whether it’s your first UTI or you’ve had several (if so, we’re so sorry!), providers may be able to help.</p>
+              <p>We’ll ask you a series of questions to see if this service is right for you. You will only proceed i we feel that this service will benefit you. If you are a good candidate.</p>
             </Card>
           </div>
         </div>
@@ -72,16 +74,16 @@ const Content = ({ navigate }) => {
         </div>
         <Collapse style={{textAlign: 'left'}} ghost>
           <Collapse.Panel header="Can I get antibiotics for a urinary tract infection (UTI) without seeing a doctor?" key="1">
-            <p>You do not need to visit a provider’s office to be prescribed antibiotics for a urinary tract infection (UTI). Instead, you can speak with a provider online from the comfort of your home, which can help you start treatment sooner, if appropriate. Online UTI Meds offers online visits for UTI prescriptions for as low as ${price}.</p>
+            <p>Now you do not need to visit a provider’s office to be prescribed antibiotics for a simple urinary tract infection (UTI). Instead, you can be put in contact with a provider online from the comfort of your home, which can help you start treatment sooner.UTIRX.com offers online visits for UTI prescriptions for as low as ${price}.</p>
           </Collapse.Panel>
           <Collapse.Panel header="When should I see a doctor for a UTI?" key="2">
-            <p>You’ll want to see a provider as soon as you notice symptoms of a urinary tract infection (UTI). A common symptom is pain or burning when you urinate, but you might also feel a frequent urge to urinate even when your bladder is empty. If you start experiencing pain in your back or side, or if you develop a fever, this might be a sign of a more serious infection in your kidneys.</p>
+            <p>You’ll want to see a provider as soon as you notice symptoms of a urinary tract infection (UTI). A common symptom is pain or burning when you urinate, but you might also feel a frequent urge to urinate even when your bladder is empty. If you start experiencing pain in your back or side, or if you develop a fever, this might be a sign of a more serious infection in your kidneys.While we always advise making + seeing a doc in person , we may be able to provide immediate Tx for simple UTIS saving you time the money.</p>
           </Collapse.Panel>
           <Collapse.Panel header="What causes a UTI?" key="3">
-            <p>Urinary tract infections (UTIs) happen when bacteria is able to get into the urinary tract, causing an infection. Different factors can increase the likelihood of this happening, like sexual activity, changes in hormone levels during menopause, pregnancy, and certain types of birth control.</p>
+            <p>Urinary tract infections (UTIs) happen when bacteria is able to get into the urinary tract, causing an infection. Different factors can increase the likelihood of this happening, like sexual activity, changes in hormone levels during menopause, pregnancy.</p>
           </Collapse.Panel>
           <Collapse.Panel header="Can a UTI go away on its own?" key="4">
-            <p>It is best to see a provider if you are experiencing symptoms of a urinary tract infection (UTI). Leaving it untreated may result in a more serious infection that spreads to your kidneys. Online UTI Meds offers online visits for UTI so that you can get started on treatment sooner.</p>
+            <p>It is best to see a provider if you are experiencing symptoms of a urinary tract infection (UTI). Leaving it untreated may result in a more serious infection that spreads to your kidneys. UTIRX.com offers online visits for UTI so that you can get started on treatment sooner.</p>
           </Collapse.Panel>
           <Collapse.Panel header="What are the symptoms of a UTI?" key="5">
             <p>A common symptom of a urinary tract infection (UTI) is a painful or burning sensation when you urinate. You might also feel a frequent urge to pee even when your bladder is empty or notice that your urine looks bloody or cloudy. Sometimes a UTI can spread to your kidneys, causing back or side pain, fever, chills, and nausea.</p>
